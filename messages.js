423a94c3040f4d934a7e1bd32a02d18623dd5be5
ii87 <ii87@ukr.net>
2020-01-30 21:15:39 +0200

    init commit

---
+++ messages.js
@@ -0,0 +1,40 @@
+var client;
+
+var connectOptions = {
+  timeout: 30,
+  reconnect: true,
+  cleanSession: false,
+  mqttVersion: 4,
+  keepAliveInterval: 10,
+  onSuccess: onConnect,
+  onFailure: onFailure
+}
+
+function connect() {
+  try {
+    client = new Paho.Client('192.168.20.20', 8083, document.forms.sender.client.value);
+    connectOptions.userName = document.forms.sender.user.value;
+    client.connect(connectOptions);
+  } catch (ex) {
+    console.log(ex);
+  }
+}
+
+function onConnect() {
+  console.log('on connect');
+  client.onMessageArrived = function(message) {
+    console.log("onMessageArrived: " + message.payloadString);
+  }
+  client.subscribe("test", { qos: 2 });
+}
+
+function onFailure(err) {
+  console.log('on failure', JSON.stringify(err));
+}
+
+function send() {
+   var message = new Paho.Message(document.forms.sender.message.value);
+   message.destinationName = "test";
+   message.qos = 2;
+   client.send(message);
+}
